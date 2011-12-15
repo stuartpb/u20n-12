62349cc7ee79f9b0bff5a937cc0a0fe5805bbbce
Stuart P. Bentley <stuart@testtrack4.com>
2011-12-15 05:49:50 -0800

    Separate local directory for redirect pres pages

--- make-redirects.lua
+++ make-redirects.lua
@@ -15,7 +15,7 @@ for tr_i=1, #tracks.names do
   local trn = tracks.names[tr_i]
   local track = tracks[trn]
   for i=1, #track do
-    local oldpage = string.format("presentations/%s%i.html",trn,i)
+    local oldpage = string.format("pres-redirs/%s%i.html",trn,i)
     local newpage = string.format("/2012/presentations/%s.html",track[i].id)
     io.output(oldpage)
     io.write((string.gsub(template,'@',newpage)))
