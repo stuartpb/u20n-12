615cd4aa86ed5c1d927076b4c9e4c1f14386982e
Stuart P. Bentley <stuart@testtrack4.com>
2011-12-15 04:46:40 -0800

    Add script to make HTML files redirecting to the new presentations

---
+++ make-redirects.lua
@@ -0,0 +1,23 @@
+local tracks = require "tracks"
+
+local template = [[
+<html>
+<head>
+<meta http-equiv="Refresh" content="0; url=@" />
+</head>
+<body>
+<p>Moved to <a href="@">@</a></p>
+</body>
+</html>
+]]
+
+for tr_i=1, #tracks.names do
+  local trn = tracks.names[tr_i]
+  local track = tracks[trn]
+  for i=1, #track do
+    local oldpage = string.format("presentations/%s%i.html",trn,i)
+    local newpage = string.format("/2012/presentations/%s.html",track[i].id)
+    io.output(oldpage)
+    io.write((string.gsub(template,'@',newpage)))
+  end
+end
