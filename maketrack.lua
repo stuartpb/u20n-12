d50c6c9c8c834a5b2a720d0ef5b77b6439be71d1
Stuart P. Bentley <stuart@testtrack4.com>
2011-12-15 04:45:38 -0800

    Change presentation IDs to be based on the title

--- maketrack.lua
+++ maketrack.lua
@@ -1,12 +1,27 @@
 local string = require 'string'
 local format = string.format
+local gsub = string.gsub
+local lower = string.lower
 local table = require 'table'
 local sort = table.sort
 
+local idset = {}
+
 return function(track, ps)
   for i=1, #ps do
-    ps[i].track=track
-    ps[i].id=format("%s%i",track,i)
+    ps[i].track = track
+    local id = ps[i].title
+    id = gsub(id,"%u",lower)
+    id = gsub(id,"&","and")
+    id = gsub(id,"[ /]","-")
+    id = gsub(id,"%-+","-")
+    id = gsub(id,"[^%w%-]","")
+    if idset[id] then
+      error("redundant id: "..id)
+    else
+      idset[id] = true
+    end
+    ps[i].id = id
   end
 
   sort(ps,function(m,n)
